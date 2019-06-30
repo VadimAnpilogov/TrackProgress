89ee993583896b4f9769350682a3edfaa7c87026
Frost98992 <denis.moroz.98@gmail.com>
2019-06-30 20:03:44 +0700

    custom html

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -2,8 +2,6 @@ package hello;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -12,10 +10,6 @@ public class GreetingController {
     @GetMapping()
     public String greeting() {
         return "greeting";
-    }
-    @PostMapping
-    public  String add(@RequestParam String fio, @RequestParam String group){
-        return "main";
     }
 
 }
