cb823ba268cc65559d0a4eb2474e2b739556b210
Frost98992 <denis.moroz.98@gmail.com>
2019-06-30 18:40:09 +0700

    add out/ to .gitignore

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -2,6 +2,8 @@ package hello;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -10,6 +12,10 @@ public class GreetingController {
     @GetMapping()
     public String greeting() {
         return "greeting";
+    }
+    @PostMapping
+    public  String add(@RequestParam String fio, @RequestParam String group){
+        return "main";
     }
 
 }
