64354b530204664b50f9d8c089ff1573beb9932b
VadimAnpilogov <vadick.anpilogov2015@yandex.ru>
2019-06-30 17:30:15 +0700

    correct html

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class GreetingController {
 
-    @GetMapping("/greeting")
+    @GetMapping()
     public String greeting() {
         return "greeting";
     }
